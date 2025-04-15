e86a2e801745488789f203dabf616639312a835c
ajin21 <3197237971@qq.com>
2025-04-15 17:15:11 +0800

    Initial commit

---
+++ assets/js/bookmarks.js
@@ -0,0 +1,33 @@
+var bookmarkData = {
+    list: [
+        {
+            title: "懂技术",
+            icon: "code",
+            child: [
+                {
+                    title: "常用推荐",
+                    nav: [
+                        {
+                            title: "微信公众平台",
+                            desc: "微信公众号内容管理平台",
+                            url: "https://mp.weixin.qq.com/",
+                            image: "mp.weixin.qq.com"
+                        },
+                        {
+                            title: "markdown | 文章排版",
+                            desc: "优雅的Markdown编辑器",
+                            url: "https://editor.mdnice.com/",
+                            image: "editor.mdnice.com"
+                        },
+                        {
+                            title: "135编辑器 | 编辑器",
+                            desc: "专业的微信排版编辑器",
+                            url: "https://www.135editor.com/",
+                            image: "www.135editor.com"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
